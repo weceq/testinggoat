d5c6a1f7ac46fbce0e1d92fbf08865dc2b14364b
Dawid Grzegorz Węckowski <weceq@tlen.pl>
2015-02-14 19:31:18 +0100

    URL, view and unit tests for new list, changed POST actions

--- lists/tests.py
+++ lists/tests.py
@@ -19,33 +19,6 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
 
-    def test_home_page_can_save_POST_request(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = "A new list item"
-
-        response = home_page(request)
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, 'A new list item')
-
-    def test_home_page_redirects_after_POST(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['item_text'] = "A new list item"
-
-        response = home_page(request)
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-unique-list-id/')
-
-    def test_home_page_only_saves_items_when_neccessary(self):
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertEqual(Item.objects.count(), 0)
-
 class ListViewTest(TestCase):
     def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
@@ -59,6 +32,25 @@ class ListViewTest(TestCase):
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-unique-list-id/')
         self.assertTemplateUsed(response, 'list.html') 
+
+class NewListTest(TestCase):
+    def test_saving_POST_request(self):
+        self.client.post(
+                '/lists/new',
+                data={'item_text': "A new list item"}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post(
+                '/lists/new',
+                data={'item_text': "A new list item"}
+        )
+
+        self.assertRedirects(response, '/lists/the-unique-list-id/')
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
--- lists/views.py
+++ lists/views.py
@@ -5,14 +5,12 @@ from lists.models import Item
 # Create your views here.
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item_text = request.POST.get('item_text', '')
-        Item.objects.create(text=new_item_text)
-        return redirect('/lists/the-unique-list-id/')
-
-    items = Item.objects.all()
     return render(request, 'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-unique-list-id/')
