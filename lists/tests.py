2b31aebcc859a6aa419c0498ca706abd2e5a7648
Dawid Grzegorz Węckowski <weceq@tlen.pl>
2015-02-14 18:49:35 +0100

    New URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -38,7 +38,7 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-unique-list-id/')
 
     def test_home_page_only_saves_items_when_neccessary(self):
         request = HttpRequest()
@@ -46,15 +46,19 @@ class HomePageTest(TestCase):
 
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_displays_alllist_items(self):
+class ListViewTest(TestCase):
+    def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/lists/the-unique-list-id/')
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertContains(response,'itemey 1')
+        self.assertContains(response,'itemey 2')
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-unique-list-id/')
+        self.assertTemplateUsed(response, 'list.html') 
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
@@ -73,3 +77,4 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
--- lists/views.py
+++ lists/views.py
@@ -8,7 +8,11 @@ def home_page(request):
     if request.method == 'POST':
         new_item_text = request.POST.get('item_text', '')
         Item.objects.create(text=new_item_text)
-        return redirect('/')
+        return redirect('/lists/the-unique-list-id/')
 
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'home.html')
+
+def view_list(request):
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
