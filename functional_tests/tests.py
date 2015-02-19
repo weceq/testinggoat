cce35d3070ef86dbe19eeb3968a193f6fa0601e6
Dawid Grzegorz Węckowski <weceq@tlen.pl>
2015-02-19 21:52:01 +0100

    New URL and view for adding items to existing list.
    FT passes.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -82,7 +82,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again there is no trace of Edith's list
-        page_text = self.browser.get_element_by_tag_name('body').text
+        page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
 
--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from lists.views import home_page
+from lists.views import home_page, add_item
 from lists.models import Item, List
 
 # Create your tests here.
@@ -20,18 +20,24 @@ class HomePageTest(TestCase):
         self.assertEqual(response.content.decode(), expected_html)
 
 class ListViewTest(TestCase):
-    def test_displays_all_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list = list_)
-        Item.objects.create(text='itemey 2', list = list_)
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list = correct_list)
+        Item.objects.create(text='itemey 2', list = correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list = other_list)
+        Item.objects.create(text='other list item 2', list = other_list)
 
-        response = self.client.get('/lists/the-unique-list-id/')
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
 
-        self.assertContains(response,'itemey 1')
-        self.assertContains(response,'itemey 2')
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-unique-list-id/')
+        list_ = List.objects.create()
+        response = self.client.get('/lists/%d/' % (list_.id,))
         self.assertTemplateUsed(response, 'list.html') 
 
 class NewListTest(TestCase):
@@ -50,8 +56,41 @@ class NewListTest(TestCase):
                 '/lists/new',
                 data={'item_text': "A new list item"}
         )
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
 
-        self.assertRedirects(response, '/lists/the-unique-list-id/')
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
+
+class NewItemTest(TestCase):
+
+    def test_can_save_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+                '/lists/%d/add_item' % (correct_list.id,),
+                data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+                '/lists/%d/add_item' % (correct_list.id,),
+                data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
 
 class ListAndItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
