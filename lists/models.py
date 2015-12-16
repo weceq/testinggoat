f405bcd547e924b01af2209371c4b4861fc2087a
Dawid Grzegorz Węckowski <weceq@tlen.pl>
2015-12-16 14:44:42 +0100

    Implement duplicate item validation at model layer

--- lists/models.py
+++ lists/models.py
@@ -1,12 +1,33 @@
-from django.db import models
+""" Model classes for list application
+"""
 from django.core.urlresolvers import reverse
+from django.db import models
 
-# Create your models here.
 
 class List(models.Model):
+
+    """List
+    """
+
     def get_absolute_url(self):
+        """Get direct url mapping for a list
+        """
         return reverse('view_list', args=[self.id])
 
+
 class Item(models.Model):
+
+    """Single item on a list
+    """
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
+
+    class Meta:
+
+        """Restrictions and settings for Items
+        """
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+
+    def __str__(self):
+        return self.text
--- lists/views.py
+++ lists/views.py
@@ -27,4 +27,3 @@ def new_list(request):
         return redirect(list_)
     else:
         return render(request, 'home.html', {'form': form})
-
