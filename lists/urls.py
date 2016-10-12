ba6be3dedf05a2102702758c938fc98b7064901f
Dawid Grzegorz Węckowski <weceq@tlen.pl>
2016-10-12 23:05:35 +0200

    Upgrade Django to 1.10.2
    Add: TEMPLATE entry to settings.py
    Mod: url patterns to adhere to new format
    Mod: requiremets.txt to Django 1.10.2

--- lists/urls.py
+++ lists/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import  url
 
-urlpatterns = patterns('',
-    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
-    url(r'^new$', 'lists.views.new_list', name='new_list'),
-)
+import lists
+
+urlpatterns = [
+    url(r'^(\d+)/$', lists.views.view_list, name='view_list'),
+    url(r'^new$', lists.views.new_list, name='new_list')
+]
--- superlists/urls.py
+++ superlists/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import  include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+import lists
+
+urlpatterns = [
     # Examples:
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls')),
+    url(r'^$', lists.views.home_page, name='home'),
+    url(r'^lists/', include('lists.urls'))
     # url(r'^blog/', include('blog.urls')),
 
     # url(r'^admin/', include(admin.site.urls)),
-)
+]
