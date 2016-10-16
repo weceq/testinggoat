edbb08e7ff260e047acd95e86e981cffad7c3176
Dawid Grzegorz Węckowski <weceq@tlen.pl>
2016-10-16 21:46:38 +0200

    Handle duplicate errors in forms
    Add: handling duplicate item errors in forms

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -47,3 +47,8 @@ class ExistingListItemFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
 
+    def test_form_save(self):
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
+        new_item = form.save()
+        self.assertEquals(new_item, Item.objects.all()[0])
