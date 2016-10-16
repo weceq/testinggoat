ba56e09d3322384ae5bd1a48b57fd4ec13767436
Dawid Grzegorz Węckowski <weceq@tlen.pl>
2016-10-16 21:35:44 +0200

    Fix typo in duplicate validation test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -41,4 +41,4 @@ class ListValidationTest(FunctionalTest):
         # She sees a helpful error message
         self.check_for_row_in_list_table("1: Buy wellies")
         error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, "You've already got rhis in your list")
+        self.assertEqual(error.text, "You've already got this in your list")
